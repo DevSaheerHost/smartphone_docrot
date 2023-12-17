06059f680f147fc84d0a39ea8975eca6a67ee39a
Saheer Babu <122529965+DevSaheerHost@users.noreply.github.com>
2023-12-17 09:51:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 console.log('Hello World!');
-const loading_container = document.querySelector('.loading-container')
+ const loading_container = document.querySelector('.loading-container')
 document.querySelector('img').onload=()=>{
   loading_container.classList='loading-container-close'
 }
 
 // star over
+
+
+loade=()=>{
+  loading_container.classList='loading-container-close'
+}
+setTimeout(loade, 4000);
